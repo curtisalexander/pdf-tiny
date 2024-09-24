51c4d800911ebbda8f53118999022cc0a5e4df58
Curtis Alexander <6118829+curtisalexander@users.noreply.github.com>
2024-09-24 14:47:04 -0500

    Add Rust implementation

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # pdf-tiny
 :crab: Rust wrapper around :ghost: GhostScript to reduce the size of PDFs
+
+## Usage
+```shell
+pdf-tiny input_file.pdf
+```
+
+Writes output file to same directory as input file, appending `_tiny` to the filename.
+
+## Requirements
+Requires [GhostScript](https://ghostscript.com/releases/gsdnld.html) to be installed.  Further assumes that GhostScript is installed within `C:\Program Files` using the default installation options.  [Adjust]() as needed.
+
+## Installation
+
+## How Is It Built?
+This is simply a :crab: Rust wrapper around a set of options to run GhostScript from the command line.
+
+The file path to GhostScript is assumed to start with `C:\Program Files\gs` and is [glob](https://docs.rs/glob/latest/glob/) matched to ignore the version number in order to identify the `gswin64c.exe` executable.  Then [xshell](https://docs.rs/xshell/latest/xshell/) is utilized to execute the program.
+
+## Why Build This?
+- Opportunity to mess around with `xtask`
+- I dislike large PDFs &mdash; they crash my printer
+
+## Inspired By
+[SmallerPDF](https://github.com/crissNb/SmallerPDF) by [crissNb](https://github.com/crissNb)
---
+++ src/main.rs
@@ -0,0 +1,49 @@
+use anyhow::{Context, Result};
+use clap::Parser;
+use glob::glob;
+use owo_colors::OwoColorize;
+use std::path::PathBuf;
+use xshell::{cmd, Shell};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Input PDF file
+    #[arg(short, long)]
+    path: std::path::PathBuf
+}
+
+fn main() -> anyhow::Result<()> {
+
+    let args = Args::parse();
+
+    let gs_pattern = "C:\\Program Files\\gs\\**\\bin";
+    let gs_path = get_first_path(&gs_pattern)?;
+    let gs_executable = gs_path.join("gswin64c.exe");
+
+    let input_file = args.path;
+    if input_file.extension() != Some(std::ffi::OsStr::new("pdf")) { anyhow::bail!("Input file provided is not a PDF (i.e. does not have a pdf extension): {:?}", input_file)};
+
+    let input_file_stem = input_file.file_stem().unwrap().to_str().unwrap();
+    let output_file = input_file.parent().unwrap().join(format!("{input_file_stem}_tiny.pdf"));
+
+    println!("Shrinking: {:?}", input_file.blue());
+    println!("Tiny version will be: {:?}", output_file.green());
+
+    let sh = Shell::new()?;
+    cmd!(sh, "{gs_executable} -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile={output_file} {input_file}").run()?;
+
+    Ok(())
+}
+
+fn get_first_path(pattern: &str) -> Result<PathBuf, anyhow::Error> {
+    let paths = glob(pattern).with_context(|| format!("Pattern error: {:?}", pattern));
+
+    for entry in paths? {
+        match entry {
+            Ok(path) => { return Ok(path.to_path_buf()) },
+            Err(e) => { anyhow::bail!("Match error: {}", e); }
+        }
+    }
+    anyhow::bail!("No path found based on pattern: {:?}", pattern);
+}
